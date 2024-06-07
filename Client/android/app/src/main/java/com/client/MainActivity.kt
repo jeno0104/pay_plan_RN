832f2edbbaf46341c008d441396099e2b9bf81d9
zenoo97 <jaehun4086@naver.com>
2024-06-07 22:03:53 +0900

    [Update] splash screen 적용 전

--- Client/android/app/src/main/java/com/client/MainActivity.kt
+++ Client/android/app/src/main/java/com/client/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.client
 
 import com.facebook.react.ReactActivity
+
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
 
+
 class MainActivity : ReactActivity() {
+  
+  
 
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
@@ -19,4 +23,5 @@ class MainActivity : ReactActivity() {
    */
   override fun createReactActivityDelegate(): ReactActivityDelegate =
       DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
+  
 }
